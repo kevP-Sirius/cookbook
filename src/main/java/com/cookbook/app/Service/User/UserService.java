9e0d7fd30423b0a3cfefb5577a44173afae95204
achot BARSEGHYAN <achot@closandmonopole.com>
2023-01-23 16:34:14 +0100

    user service

--- src/main/java/com/cookbook/app/Service/User/UserService.java
+++ src/main/java/com/cookbook/app/Service/User/UserService.java
@@ -1,12 +1,28 @@
 package com.cookbook.app.Service.User;
 
 import com.cookbook.app.Entity.Recipe;
+import com.cookbook.app.Entity.User;
 import com.cookbook.app.Repository.UserRepository;
+
+import java.util.List;
 
 public class UserService implements IUserService {
     private final UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public User createUser(User user){
+        return userRepository.save(user);
+    }
+
+    public boolean deleteUser(User user){
+        userRepository.delete(user);
+        return true;
     }
 }
